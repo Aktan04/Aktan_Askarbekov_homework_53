9b6f7aaee348783fc4b03b55a877d13ac2be3812
Askarbekov Aktan <akn392204@gmail.com>
2024-04-18 15:13:24 +0600

    Add some changes in layout, change master page

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Phone}/{action=Index}/{id?}");
 
 app.Run();
