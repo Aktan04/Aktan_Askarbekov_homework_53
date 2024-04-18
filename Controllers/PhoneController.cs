608e6f2960267e0f83b44c58cc34ca32fdba55b3
Askarbekov Aktan <akn392204@gmail.com>
2024-04-18 14:27:03 +0600

    Add Edit and Delete actions for PhoneController

--- Controllers/PhoneController.cs
+++ Controllers/PhoneController.cs
@@ -34,4 +34,57 @@ public class PhoneController : Controller
         
         return RedirectToAction("Index");
     }
+    
+    public IActionResult Edit(int? id)
+    {
+        if (id != null)
+        {
+            Phone phone = _db.Phones.FirstOrDefault(p => p.Id == id);
+            if (phone != null)
+            {
+                return View(phone);
+            }
+        }
+        return NotFound();
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Phone phone)
+    {
+        if (phone != null)
+        {
+            _db.Phones.Update(phone);
+            _db.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
+    
+    public IActionResult Delete(int? id)
+    {
+        if (id != null)
+        {
+            Phone phone = _db.Phones.FirstOrDefault(p => p.Id == id);
+            if (phone != null)
+            {
+                return View(phone);
+            }
+        }
+        return NotFound();
+    }
+    
+    [HttpPost]
+    public IActionResult ConfirmDelete(int? id)
+    {
+        if (id != null)
+        {
+            Phone phone = _db.Phones.FirstOrDefault(p => p.Id == id);
+            if (phone != null)
+            {
+                _db.Phones.Remove(phone);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        return NotFound();
+    }
 }
